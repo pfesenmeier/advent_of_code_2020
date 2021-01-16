6bb7ac868f5e5803c2e0f48d2b49cf03a77303aa
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-01-16 15:10:24 -0500

    Parse problem as 1_1, 1-2, etc..

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,63 @@
 use std::env;
-use std::char;
-use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let filename = &args[1];
-    let input = fs::read_to
+    let challenge = Challenge::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    parse_name(filename);
+    challenge.run();
 }
 
-enum Part {
-    A,
-    B,
-}
-struct Solution {
+struct Challenge<'a> {
     day: u8,
-    part: Part,
+    part: u8,
+    input_name: &'a str,
 }
-fn parse_name(file_name: &str) -> Result<Solution, > {
-//   if file_name.len() != 3 { return None };
 
-  let day: u8 = file_name[0..2].parse().expect("Failed to parse a day.")?;
-  println!("{}", day);
+impl Challenge<'_> {
+    fn new(args: &[String]) -> Result<Challenge, &'static str> {
+        match args.len() {
+            3 => {
+              let nums: Vec<&str> = args[1].split(|input: char| { !input.is_digit(10)}).collect();
+              let day: u8 = nums[0].parse().expect("Could not parse a day from arguments");
+              let part: u8 = nums[1].parse().expect("Could not parse a part from arguments");
+              Ok(Challenge {
+                  day,
+                  part,
+                  input_name: &args[2],
+              })
+            },
+            _ => Err("Please provide two arguments"),
+        }
+    }
+
+    fn run(&self) {
+        let problem_func = self
+            .get_problem_func()
+            .expect("Could not match problem argument. Try 'day-part' e.g. '1-2'");
+        let file = self.get_file_location();
+
+        let result = problem_func(file);
+
+        println!("The answer to day {} part {} is {}", self.day, self.part, &result);
+    }
+
+    fn get_file_location(&self) -> &str {
+        self.input_name
+    }
+
+    fn get_problem_func(&self) -> Option<impl Fn(&str) -> &str> {
+        match (self.day, self.part) {
+            (1, 1) => Some(report_repair_1),
+            _ => None,
+        }
+    }
+}
+
+fn report_repair_1(_file_name: &str) -> &str {
+    "42"
 }
