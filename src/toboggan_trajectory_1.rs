83e2291aab904166d953ceb7393a9dd94978d3e7
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-01-17 23:15:18 -0500

    refactor part 1

--- src/toboggan_trajectory_1.rs
+++ src/toboggan_trajectory_1.rs
@@ -1,27 +1,36 @@
 pub fn run(file: &str) -> String {
-    fn sled(hills: &[&str], pos: u32, trees: u32) -> u32 {
+  
+  let hills: Vec<&str> = file.split_ascii_whitespace().collect();
+  let ski = Ski { right: 3, down: 1 };
+  ski.sled(&hills, 0, 0).to_string()
+}
+pub struct Ski {
+    right: u32,
+    down: usize,
+}
+
+impl Ski {
+    fn sled(&self, hills: &[&str], pos: u32, trees: u32) -> u32 {
         let hill = hills.into_iter().next();
 
         if let None = hill {
-          return trees;
+            return trees;
         }
-        
+
         let hill = hill.unwrap();
-        let downhill = &hills[1..];
-        let next_pos = || (pos + 3) % hill.len() as u32;
+        let downhill = &hills[self.down..];
+        let next_pos = || (pos + self.right) % hill.len() as u32;
 
         let mut i = 0;
         for c in hill.chars() {
             if i == pos && c == '#' {
-                return sled(downhill, next_pos(), trees + 1);
+                return self.sled(downhill, next_pos(), trees + 1);
             } else if i == pos {
-                return sled(downhill, next_pos(), trees);
+                return self.sled(downhill, next_pos(), trees);
             }
             i += 1;
         }
         // should never reach here
         42
     }
-    let hills: Vec<&str> = file.split_ascii_whitespace().collect();
-    sled(&hills, 0, 0).to_string()
 }
